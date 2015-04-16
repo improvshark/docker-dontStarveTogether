ab9fd839dfca31e538c3630180c1584c62efe15a
improvshark <improvshark@users.noreply.github.com>
2015-04-16 15:55:19 -0600

    added an update

--- srv.sh
+++ srv.sh
@@ -4,9 +4,11 @@ CONTAINERPORT=10999
 USERNAME=improvshark
 NAME=dont_starve_together
 
+function update {
+    docker run  -i -t --volumes-from $NAME $USERNAME/$NAME /opt/steamcmd/steamcmd.sh +login anonymous +force_install_dir /opt/dontStarveTogether +app_update 343050  validate +quit
+}
 
 function launch {
-  docker rm $NAME
   docker run  --name $NAME  -i -t -p $CONTAINERPORT:$HOSTPORT $USERNAME/$NAME
 }
 
@@ -15,17 +17,21 @@ function build {
 }
 
 case $1 in
+  'update')
+        echo "updating!"
+        update
+  ;;
   'build')
-  echo "building!"
-  build
+        echo "building!"
+        build
   ;;
   'start'|'launch')
-  echo "launching!"
-  launch
+        echo "launching!"
+        launch
   ;;
   ''|'-h'|'help'|'*')
-  echo "usage:  srv.sh <option>"
-  echo "  build"
-  echo "  launch"
+        echo "usage:  srv.sh <option>"
+        echo "  build"
+        echo "  launch"
   ;;
 esac
